fbe3838dc972d079b0271ddf793bc07441eca1b8
jj.kim4023 <jj.kim4023@gmail.com>
2024-06-11 23:39:06 +0900

    [STUDY] -회원가입 기능

---
+++ src/main/java/com/example/member/dto/MemberDTO.java
@@ -0,0 +1,18 @@
+package com.example.member.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class MemberDTO {
+    private long id;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+}
